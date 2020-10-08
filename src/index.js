65b0729969408d37f70f7d39de961a9011ab70f2
qinjialei <qinjialei@foxmail.com>
2020-10-08 21:46:41 +0800

    Update

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
-console.log(222);
+const attribute = /^\s*([^\s"'<>\/=]+)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s"'=<>`]+)))?/
+const dynamicArgAttribute = /^\s*((?:v-[\w-]+:|@|:|#)\[[^=]+?\][^\s"'<>\/=]*)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s"'=<>`]+)))?/
+const ncname = '[a-zA-Z_][\\w\\-\\.]*'
+const qnameCapture = `((?:${ncname}\\:)?${ncname})`
+const startTagOpen = new RegExp(`^<${qnameCapture}`)
+const startTagClose = /^\s*(\/?)>/
+const endTag = new RegExp(`^<\\/${qnameCapture}[^>]*>`)
+const doctype = /^<!DOCTYPE [^>]+>/i
+// #7298: escape - to avoid being passed as HTML comment when inlined in page
+const comment = /^<!\--/
+const conditionalComment = /^<!\[/
 
 export function htmlParser(html) {
   return html;
 }
+
+console.log(333);
