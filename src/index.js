3255bf0bc46a6fcbdd2cdbc2751554e4b2a42144
qinjialei <qinjialei@foxmail.com>
2020-10-08 22:10:29 +0800

    Update

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-const attribute = /^\s*([^\s"'<>\/=]+)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s"'=<>`]+)))?/
-const dynamicArgAttribute = /^\s*((?:v-[\w-]+:|@|:|#)\[[^=]+?\][^\s"'<>\/=]*)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s"'=<>`]+)))?/
-const ncname = '[a-zA-Z_][\\w\\-\\.]*'
-const qnameCapture = `((?:${ncname}\\:)?${ncname})`
-const startTagOpen = new RegExp(`^<${qnameCapture}`)
-const startTagClose = /^\s*(\/?)>/
-const endTag = new RegExp(`^<\\/${qnameCapture}[^>]*>`)
-const doctype = /^<!DOCTYPE [^>]+>/i
+const attribute = /^\s*([^\s"'<>\/=]+)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s"'=<>`]+)))?/;
+// 标签名
+const ncname = '[a-zA-Z_][\\w\\-\\.]*';
+const qnameCapture = `((?:${ncname}\\:)?${ncname})`;
+
+// <div
+const startTagOpen = new RegExp(`^<${qnameCapture}`);
+// > 或者 />
+const startTagClose = /^\s*(\/?)>/;
+// </div>
+const endTag = new RegExp(`^<\\/${qnameCapture}[^>]*>`);
+const doctype = /^<!DOCTYPE [^>]+>/i;
 // #7298: escape - to avoid being passed as HTML comment when inlined in page
-const comment = /^<!\--/
-const conditionalComment = /^<!\[/
+const comment = /^<!\--/;
+const conditionalComment = /^<!\[/;
+
+const html = `<div class="div" id="app" style="color:red;font-size:18px">
+  hello,world{{data}}
+  <p>测试
+    <ul>
+      <li>1</li>
+      <li>2</li>
+    </ul>
+  </p>
+</div>`;
 
 export function htmlParser(html) {
   return html;
 }
 
-console.log(333);
+export function createASTElement(tagName, attrs) {
+  return {
+    tag: tagName,
+    attrs,
+    children: [],
+  };
+}
+
+htmlParser(html);
--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,7 @@
-import { htmlParser } from './index';
+import { htmlParser, createASTElement } from './index';
 
 test('html string to ast', () => {
-  const html = `<div class="div">
+  const html = `<div class="div" id="app" style="color:red;font-size:18px">
   hello,world{{data}}
   <p>测试
     <ul>
@@ -12,4 +12,13 @@ test('html string to ast', () => {
 </div>`;
 
   expect(htmlParser(html)).toBe(html);
+});
+
+test('createASTElement', () => {
+  const res = createASTElement('div', { id: 'app' });
+  expect(res).toStrictEqual({
+    tag: 'div',
+    attrs: { id: 'app' },
+    children: [],
+  });
 });
